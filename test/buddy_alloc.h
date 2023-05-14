d5f236ded5c6dae42237bbdd04ca10974bf47b9c
Kirill Sokolov <kirill141103@yandex.ru>
2023-05-14 14:30:07 +0300

    Начал делать buddy_allocator.
    Пока я делаю его отдельно от xv6, а когда протестирую - добавлю в ядро.

---
+++ test/buddy_alloc.h
@@ -0,0 +1,88 @@
+#include "inttypes.h"
+
+
+#ifndef BUDDY_LEVELS
+#define BUDDY_LEVELS 10 
+#endif
+
+
+#ifndef BUDDY_PGSIZE
+#define BUDDY_PGSIZE 4096
+#endif
+
+
+void assert(int condition, const char* message);
+
+
+/*
+Все свободные куски одного размера соединены в связный список.
+Итого есть по одному списку для каждого размера.
+В начале каждого свободного куска лежит узел списка.
+Свободные куски в каждом списке расположены не обязательно по порядку.
+Также список хранит свою длину.
+*/
+typedef struct {
+    buddy_node_t* next;
+} buddy_node_t;
+
+typedef struct {
+    buddy_node_t* first;
+    uint64_t len;
+} buddy_list_t;
+
+
+
+/*
+В первых нескольких страницах хранятся метаданные:
+    - списки свободных кусков (поле lists)
+    - таблица состояний (поле state_table)
+Остальные страницы рабочие.
+
+Общая логика такая: для выделений и быстрого поиска свободных
+кусков используем списки lists, а для освобождений и проверки
+состояния используем state_table. 
+В результате все операции работают за константу.
+
+Таблица состояний позволяет по данному номеру страницы 
+сразу определить, что в ней.
+У первой рабочей страницы номер 0.
+Пусть номер данной страницы равен n.
+Тогда есть такие варианты: 
+    1) state_table[n] == -1   =>  эта страница свободна, 
+    2) state_table[n] == -2  =>  эта страница занята, но не является
+        первой в каком-то выделенном куске
+    3) state_table[n] == lvl >= 0  =>  эта страница занята и является
+        первой в выделенном куске уровня lvl (и размера 1 << lvl страниц)
+*/
+typedef struct {
+    int levels;
+    uint64_t pgsize;
+    buddy_list_t* lists;
+    char* state_table;
+
+    void* data;
+    uint64_t pages;
+} buddy_allocator_t;
+
+
+
+
+
+void buddy_init(
+    buddy_allocator_t* mem, 
+    int levels, uint64_t pgsize,  // гиперпараметры 
+    uint64_t pages, void* ptr     // распределяемые ресурсы
+);
+
+void* buddy_alloc(buddy_allocator_t* mem, uint64_t pages);
+void buddy_free(buddy_allocator_t* mem, void* addr);
+
+
+
+typedef struct {
+    uint64_t total;
+    uint64_t free;
+    uint64_t free_by_size[BUDDY_LEVELS];
+} buddy_info_t;
+
+void buddy_info(buddy_allocator_t* mem, buddy_info_t* info);
---
+++ test/test.cpp
@@ -0,0 +1,43 @@
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "buddy_alloc.h"
+#include <cstdio>
+
+int round_up(int x, int n){
+    int r = x % n;
+    if(r == 0)
+        return x;
+    return x - r + n;
+}
+
+TEST_CASE("init"){
+    #define TEST_INIT(_levels, _pgsize, _pages) do{ \
+        int serv_data = sizeof(buddy_list_t) * _levels + _pages;\
+        int serv_pages = round_up(serv_data, _pgsize) / _pgsize;\
+        printf(\
+            "Test init(levels = %d, pgsize = %d, pages = %d) \
+            serv_data = %d bytes, %d pages", \
+            _levels, _pgsize, _pages, serv_data, serv_pages \
+        );\
+        buddy_allocator_t mem; \
+        void* ptr = malloc(_pgsize * _pages);  \
+        buddy_init(&mem, _levels, _pgsize, _pages, ptr); \
+        REQUIRE_EQ(mem.levels, _levels); \
+        REQUIRE_EQ(mem.pgsize, _pgsize); \
+        REQUIRE_EQ(mem.pages, _pages); \       
+        REQUIRE_EQ( \
+            (char*)mem.data - (char*)ptr, \
+            serv_pages * _pgsize \
+        ); \
+        free(ptr); \
+    }while(0)
+
+    TEST_INIT(1, 110, 1000);
+    TEST_INIT(10, 110, 1000);
+
+
+    #undef TEST_INIT
+
+}
