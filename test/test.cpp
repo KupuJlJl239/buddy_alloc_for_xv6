8b1e1671a058a0a4bc7c0ed379cd4dda1098abc5
Kirill Sokolov <kirill141103@yandex.ru>
2023-05-17 22:53:27 +0300

    Написал тесты на случай неудачного выделения памяти

--- test/test.cpp
+++ test/test.cpp
@@ -7,15 +7,58 @@ extern "C"{
 }
 
 #include <cstdio>
+#include <cassert>
+
+int get_page_number(buddy_allocator_t* mem, void* page_ptr){
+    int d = (char*)page_ptr - (char*)mem->data;
+    if(d % mem->pgsize != 0)
+        return -1;
+    return d / mem->pgsize;
+}
+
+// Проверяет соответствие списков таблице состояний, а также отсутствие наложений в списках
+int check(buddy_allocator_t* mem){
+    char* my_state_table = (char*) malloc(mem->pages);
+    for(int i = 0; i < mem->pages; i++){
+        my_state_table[i] = BUDDY_USED;
+    }
+    for(int lvl = 0; lvl < mem->levels; lvl++){
+        buddy_node_t* curr = mem->lists[lvl].first;
+        while(curr != nullptr){
+            int page_number = get_page_number(mem, (void*)curr);
+            assert(page_number != -1);
+            for(int i = page_number; i < page_number + (1<<lvl); i++){
+                if(my_state_table[i] == BUDDY_FREE){
+                    printf("free page collision: page %d", i);
+                    free(my_state_table);
+                    return -1;
+                }
+                my_state_table[i] = BUDDY_FREE;
+            }
+            curr = curr->next;
+        }
+    }
+    for(int i = 0; i < mem->pages; i++){
+        char s = mem->state_table[i];
+        char ms = my_state_table[i];
+        if(
+            !(
+                s == BUDDY_FREE && ms == BUDDY_FREE || 
+                (s == BUDDY_USED || s >= 0) && ms == BUDDY_USED
+            )
+        ){
+            printf("error in page %d", i);
+            free(my_state_table);
+            return -1;
+        }
+    }
+    free(my_state_table);
+    return 0;
+}
 
 
 TEST_CASE("init"){
     #define TEST_INIT(_levels, _pgsize, _pages, _serv_pages, ...) do{ \
-        printf(\
-            "Test init(levels = %d, pgsize = %d, pages = %d) \
-            serv_data =? %d pages", \
-            _levels, _pgsize, _pages, _serv_pages \
-        );\
         buddy_allocator_t mem; \
         void* ptr = malloc(_pgsize * _pages);  \
         buddy_init(&mem, _levels, _pgsize, _pages, ptr); \
@@ -29,12 +72,41 @@ TEST_CASE("init"){
         uint64_t free_by_size[] = __VA_ARGS__;\
         for(int i = 0; i < _levels; i++)\
             CHECK_EQ(free_by_size[i], mem.lists[i].len);\
+        CHECK_EQ(check(&mem), 0);\
         free(ptr); \
     }while(0)
 
     TEST_INIT(1, 100, 1000, 11, {1000 - 11});
-    TEST_INIT(2, 100, 1000, 11, {1, 494});  // 494 = (1000-11)/2
+    TEST_INIT(2, 100, 1000, 11, {1, 494});          // 494 = (1000-11)/2
     TEST_INIT(3, 100, 1000, 11, {1, 0, 494 / 2});
 
     #undef TEST_INIT
+}
+
+
+TEST_CASE("bad_alloc"){
+    #define TEST_BAD_ALLOC(_levels, _pgsize, _pages, _pages_to_alloc) do{ \
+        buddy_allocator_t mem; \
+        void* ptr = malloc(_pgsize * _pages);  \
+        buddy_init(&mem, _levels, _pgsize, _pages, ptr); \
+        REQUIRE_EQ(buddy_alloc(&mem, _pages_to_alloc), nullptr); \
+        CHECK_EQ(check(&mem), 0);\
+        free(ptr); \
+    }while(0)
+
+    for(int lvls = 1; lvls < 10; lvls++){
+        TEST_BAD_ALLOC(lvls, 100, 1000, 0);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 3);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 5);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 6);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 7);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 9);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 1023);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 1024);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 1025);
+        TEST_BAD_ALLOC(lvls, 100, 1000, 2048);
+    }
+
+    #undef TEST_BAD_ALLOC
+
 }
